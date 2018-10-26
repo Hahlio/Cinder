fdb42d88d3246f89a57c8396872c0a6e7fc639ab
hahlio <stripey-ogima@hotmail.com>
2018-10-25 17:29:17 -0700

    Added delete and documented the code

--- cinderServer/userprofile/views.py
+++ cinderServer/userprofile/views.py
@@ -1,48 +1,82 @@
+"""
+Views used for accessing the representation of Profiles.
+The details of the profile can be accessed from here, as well
+as searching for the ID of the profile and creation of profiles.
+"""
 from threading import Thread
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse, JsonResponse
-from Matchmaking.models import updateMatch, createMatch
+from django.http import JsonResponse
+from django.db.models import Q
+from Matchmaking.models import Match, updateMatch, createMatch
 
 from .models import Profile, modifyProfile, createProfile, findID
 
 def profDetails(request, profile_id):
-    if request.method == 'GET':
-        try:
-            p = Profile.objects.get(pk=profile_id)
-        except ObjectDoesNotExist:
-            retval1 = HttpResponse("User does not Exist", 404)
-            return retval1
-        retval = p.inJson()
-        return JsonResponse(retval)
+    """
+    Methods used when the profile already exists
+    GET: gets the profile details
+    PUT: updates the profile (MUST SEND VALUES WHICH ARE NOT CHANGED ALSO)
+    DELETE: removes the profile and also the matches associated with it 
+    """
+    # Tries to retreive the profile before executing anything
+    code = 200
+    try:
+        p = Profile.objects.get(pk=profile_id)
 
-    if request.method == 'PUT':
-        retval = modifyProfile(request.body, profile_id)
-        # Updates the matchmaking algorithm in the background
-        thread = Thread(target=updateMatch, args=(Profile.objects.get(pk=retval["id"]),))
-        thread.start()
-    else:
-        retval = HttpResponse("Invalid request")
-        retval.status_code = 400
-        return retval
-    return JsonResponse(retval)
+        if request.method == 'GET':
+            retval = p.inJson()
+            return JsonResponse(retval)
+        elif request.method == 'PUT':
+            retval = modifyProfile(request.body, profile_id)
+            # Updates the matchmaking algorithm in the background
+            thread = Thread(target=updateMatch, args=(Profile.objects.get(pk=retval["id"]),))
+            thread.start()
+        elif request.method == 'DELETE':
+            Match.objects.all().filter(Q(user1__exact=p)|Q(user2__exact=p)).delete()
+            p.delete()
+            retval = {}
+        else:
+            code = 405
+            retval = {}
+            retval["status"] = 405
+            retval["userMessage"] = "The requested method is not allowed"
+
+    except ObjectDoesNotExist:
+        code = 404
+        retval = {}
+        retval["status"] = 404
+        retval["userMessage"] = "The profile you requested does not exist"
+    return JsonResponse(retval, status=code)
 
 def createProf(request):
+    """
+    Methods used when the profile doesn't exist
+    POST: Used to create a new profile 
+    """
+    code = 200
     if request.method == 'POST':
         retval = createProfile(request.body)
         # Updates the matchmaking algorithm in the background
-        thread = Thread(target=createMatch, args=(Profile.objects.get(pk=retval["id"]), ) )
-        thread.start()
-        return JsonResponse(retval)
+        thread = Thread(target=createMatch, args=(Profile.objects.get(pk=retval["id"]),))
+        thread.start()    
     else:
-        retval = HttpResponse("Invalid request")
-        retval.status_code = 400
-        return retval
+        code = 404
+        retval = {}
+        retval["status"] = 404
+        retval["userMessage"] = "The requested method is not allowed"
+    return JsonResponse(retval, status=code)
 
 def lookupUser(request, user):
+    """
+    Methods used when the profile ID isn't known
+    GET: Used to find the ID of the username
+    """
+    code = 200
     if request.method == 'GET':
         retval = findID(user)
-        return JsonResponse(retval)
     else:
-        retval = HttpResponse("Invalid request")
-        retval.status_code = 400
-        return retval
+        code = 404
+        retval = {}
+        retval["status"] = 404
+        retval["userMessage"] = "The requested method is not allowed"
+    return JsonResponse(retval, status=code)
