97b9f2698c7149ec59aaa988bb51cd0271daef85
Ubuntu <Yamsoft@YamsoftInc.zcznxxdks0aerpxkvkhmxifsgc.dx.internal.cloudapp.net>
2018-11-18 01:18:41 +0000

    Fixed travis file again

--- cinderServer/Matchmaking/tests.py
+++ cinderServer/Matchmaking/tests.py
@@ -297,7 +297,7 @@ class TestModels:
             createMatch(Profile.objects.get(pk=uid[x]))
         
         matchList = returnListOfMatches(uid[0])
-        assert len(matchList["Matches"]) == listAmount
+        assert len(matchList["Matches"]) == listAmount-1
 
         testIDs = {}
         for x in range(0, listAmount - 1):
@@ -308,7 +308,7 @@ class TestModels:
         createMatch(Profile.objects.get(pk=uid[6]))
             
         matchList = returnListOfMatches(uid[0])
-        assert len(matchList["Matches"]) == listAmount
+        assert len(matchList["Matches"]) == listAmount-1
 
         assert testIDs[0] == matchList["Matches"][0]
 
