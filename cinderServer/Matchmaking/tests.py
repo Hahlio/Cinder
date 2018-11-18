1ca8a22fbff0c173b28f845e37d3a7f91b4031de
Ubuntu <Yamsoft@YamsoftInc.zcznxxdks0aerpxkvkhmxifsgc.dx.internal.cloudapp.net>
2018-11-18 00:11:07 +0000

    Added everDjango testcases almost done with travis file

--- cinderServer/Matchmaking/tests.py
+++ cinderServer/Matchmaking/tests.py
@@ -1,9 +1,265 @@
 from django.urls import reverse,resolve
+from userprofile.models import Profile, createProfile
+from .models import returnMatch, returnListOfMatches, createMatch, updateMatch
 
-class TestMatchmaking:
+import pytest
+
+
+class TestMatchURL:
 
     def test_Matches_url(self):
         path = reverse('matchmaking', kwargs={'profile_id' : 1})
         assert resolve(path).view_name == 'matchmaking'
 
+diffProfile = {}
+diffProfile["name"] = "Doesn't Matter"
+diffProfile["username"] = "DiffTest2"
+diffProfile["password"] = "test"
+diffProfile["deviceid"] = "Android5"
+diffProfile["lat"] = 80.000
+diffProfile["lng"] = 111.123
+diffProfile["school"] = "International School"
+diffProfile["courses"] = "CPSC 221"
+diffProfile["preferences"] = "Group Discussion"
+diffProfile["interests"] = "Animal Videos"
+
+testProfiles = {}
+
+#test profile 1
+testProfiles[0] = {}
+testProfiles[0]["name"] = "Doesn't Matter"
+testProfiles[0]["username"] = "test1"
+testProfiles[0]["password"] = "test"
+testProfiles[0]["deviceid"] = "Android1"
+testProfiles[0]["lat"] = 80.000
+testProfiles[0]["lng"] = 112.123
+testProfiles[0]["school"] = "Plebian school"
+testProfiles[0]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[0]["preferences"] = "Group Discussion"
+testProfiles[0]["interests"] = "Animal Videos"
+
+#test profile 2
+testProfiles[1] = {}
+testProfiles[1]["name"] = "Doesn't Matter"
+testProfiles[1]["username"] = "test2"
+testProfiles[1]["password"] = "test"
+testProfiles[1]["deviceid"] = "Android1"
+testProfiles[1]["lat"] = 20.321
+testProfiles[1]["lng"] = 80.000
+testProfiles[1]["school"] = "Plebian school"
+testProfiles[1]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[1]["preferences"] = "Group Discussion"
+testProfiles[1]["interests"] = "Animal Videos"
+
+
+#test profile 3
+testProfiles[2] = {}
+testProfiles[2]["name"] = "Doesn't Matter"
+testProfiles[2]["username"] = "test3"
+testProfiles[2]["password"] = "test"
+testProfiles[2]["deviceid"] = "Android1"
+testProfiles[2]["lat"] = 56.321
+testProfiles[2]["lng"] = 100.000
+testProfiles[2]["school"] = "Plebian school"
+testProfiles[2]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[2]["preferences"] = "Group Discussion"
+testProfiles[2]["interests"] = "Animal Videos"
+
+
+#test profile 4
+testProfiles[3] = {}
+testProfiles[3]["name"] = "Doesn't Matter"
+testProfiles[3]["username"] = "test4"
+testProfiles[3]["password"] = "test"
+testProfiles[3]["deviceid"] = "Android1"
+testProfiles[3]["lat"] = 55.321
+testProfiles[3]["lng"] = 25.123
+testProfiles[3]["school"] = "Plebian school"
+testProfiles[3]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[3]["preferences"] = "Group Discussion"
+testProfiles[3]["interests"] = "Animal Videos"
+
+
+#test profile 5
+testProfiles[4] = {}
+testProfiles[4]["name"] = "Doesn't Matter"
+testProfiles[4]["username"] = "test5"
+testProfiles[4]["password"] = "test"
+testProfiles[4]["deviceid"] = "Android1"
+testProfiles[4]["lat"] = 33.321
+testProfiles[4]["lng"] = 69.123
+testProfiles[4]["school"] = "Plebian school"
+testProfiles[4]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[4]["preferences"] = "Group Discussion"
+testProfiles[4]["interests"] = "Animal Videos"
+
+
+#test profile 6
+testProfiles[5] = {}
+testProfiles[5]["name"] = "Doesn't Matter"
+testProfiles[5]["username"] = "test6"
+testProfiles[5]["password"] = "test"
+testProfiles[5]["deviceid"] = "Android1"
+testProfiles[5]["lat"] = 11.321
+testProfiles[5]["lng"] = 88.420
+testProfiles[5]["school"] = "Plebian school"
+testProfiles[5]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[5]["preferences"] = "Group Discussion"
+testProfiles[5]["interests"] = "Animal Videos"
+
+
+#test profile 7
+testProfiles[6] = {}
+testProfiles[6]["name"] = "Doesn't Matter"
+testProfiles[6]["username"] = "test7"
+testProfiles[6]["password"] = "test"
+testProfiles[6]["deviceid"] = "Android1"
+testProfiles[6]["lat"] = -50.321
+testProfiles[6]["lng"] = 90.000
+testProfiles[6]["school"] = "Plebian school"
+testProfiles[6]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[6]["preferences"] = "Group Discussion"
+testProfiles[6]["interests"] = "Animal Videos"
+
+
+#test profile 8
+testProfiles[7] = {}
+testProfiles[7]["name"] = "Doesn't Matter"
+testProfiles[7]["username"] = "test8"
+testProfiles[7]["password"] = "test"
+testProfiles[7]["deviceid"] = "Android1"
+testProfiles[7]["lat"] = 90.321
+testProfiles[7]["lng"] = 120.00
+testProfiles[7]["school"] = "Plebian school"
+testProfiles[7]["courses"] = "CPEN311"
+testProfiles[7]["preferences"] = "Group Discussion"
+testProfiles[7]["interests"] = "Animal Videos"
+
+
+#test profile 9
+testProfiles[8] = {}
+testProfiles[8]["name"] = "Doesn't Matter"
+testProfiles[8]["username"] = "test9"
+testProfiles[8]["password"] = "test"
+testProfiles[8]["deviceid"] = "Android1"
+testProfiles[8]["lat"] = 0.321
+testProfiles[8]["lng"] = 0.123
+testProfiles[8]["school"] = "Plebian school"
+testProfiles[8]["courses"] = "CPEN311, CPEN321, CPEN 331"
+testProfiles[8]["preferences"] = "Group Discussion"
+testProfiles[8]["interests"] = "Animal Videos"
+
+
+#test profile 10
+testProfiles[9] = {}
+testProfiles[9]["name"] = "Doesn't Matter"
+testProfiles[9]["username"] = "test10"
+testProfiles[9]["password"] = "test"
+testProfiles[9]["deviceid"] = "Android1"
+testProfiles[9]["lat"] = -10.321
+testProfiles[9]["lng"] = -110.123
+testProfiles[9]["school"] = "Plebian school"
+testProfiles[9]["courses"] = "CPEN311, CPEN321, ELEC221, CPEN 331"
+testProfiles[9]["preferences"] = "Group Discussion"
+testProfiles[9]["interests"] = "Animal Videos"
+
+
+#test profile 11
+testProfiles[10] = {}
+testProfiles[10]["name"] = "Doesn't Matter"
+testProfiles[10]["username"] = "test11"
+testProfiles[10]["password"] = "test"
+testProfiles[10]["deviceid"] = "Android1"
+testProfiles[10]["lat"] = 23.321
+testProfiles[10]["lng"] = 34.123
+testProfiles[10]["school"] = "Plebian school"
+testProfiles[10]["courses"] = "CPEN311, CPEN321"
+testProfiles[10]["preferences"] = "Quiet Study"
+testProfiles[10]["interests"] = "Animal Videos"
+
+
+@pytest.mark.django_db
+class TestModels:
+    
+# User is added into the database and receives a reasonable score with another user. (Testing Core Functionality)
+    def test_add_new_positive(self):
+        test0 = createProfile(testProfiles[0])
+        uid0 = test0["id"]
+        createMatch(Profile.objects.get(pk=uid0))
+
+        test1 = createProfile(testProfiles[1])
+        uid1 = test1["id"]
+        createMatch(Profile.objects.get(pk=uid1))
+        matchList = returnListOfMatches(uid0)
+
+        assert len(matchList["Matches"]) == 1
+        assert matchList["Matches"][0] == uid1
         
+
+# User is added into the database but doesn’t match with another User (Testing Core Functionality)
+    def test_add_new_negative(self):
+        test0 = createProfile(testProfiles[0])
+        uid0 = test0["id"]
+        createMatch(Profile.objects.get(pk=uid0))
+
+        test1 = createProfile(diffProfile)
+        uid1 = test1["id"]
+        createMatch(Profile.objects.get(pk=uid1))
+        matchList = returnListOfMatches(uid0)
+
+        assert len(matchList["Matches"]) == 0
+
+# Application requests matches and receives 10 results each time (Testing Core functionality and reliability)
+    def test_ten_matches(self):
+        test = {}
+        uid = {}
+        for x in range(0, 10):
+            test[x] = createProfile(testProfiles[x])
+            uid[x] = test[x]["id"]
+            createMatch(Profile.objects.get(pk=uid[x]))
+        
+        matchList = returnListOfMatches(uid[0])
+        assert len(matchList["Matches"]) == 5
+
+        matchList = returnListOfMatches(uid[0])
+        assert len(matchList["Matches"]) == 5
+
+        matchList = returnListOfMatches(uid[0])
+        assert len(matchList["Matches"]) == 5
+
+        matchList = returnListOfMatches(uid[0])
+        assert len(matchList["Matches"]) == 5
+
+
+# Application requests matches and the 10 results are decreasing from the highest score (Testing Core functionality)
+    def test_match_dec(self):
+        test = {}
+        uid = {}
+        for x in range(0, 11):
+            test[x] = createProfile(testProfiles[x])
+            uid[x] = test[x]["id"]
+            createMatch(Profile.objects.get(pk=uid[x]))
+        
+        matchList = returnListOfMatches(uid[0])
+        assert len(matchList["Matches"]) == 5
+        
+        testIDs = {}
+        
+
+
+"""
+# Application request for matches and receives 10 same matches. (Tests that matches won’t change if user hasn’t gone through them)
+    def test_same_matches(self):
+
+# Application request for matches and receives 10 different matches. (Tests that if new users are added or user has accepted/declined matches, his match list is updated.)
+    def test_diff_matches(self):
+
+# Application accepts the match (Tests core functionality)
+    def test_accept_matches(self):
+
+# Application declines the match (Tests core functionality)
+    def test_decline_matches(self):
+        
+"""
+
+
--- cinderServer/userprofile/tests.py
+++ cinderServer/userprofile/tests.py
@@ -1,24 +1,232 @@
-from django.db import models
-from .models import Profile, createProfile
+from .models import Profile, createProfile, modifyProfile, findID, validID
+from mixer.backend.django import mixer
+
 import pytest
+
+# Test arguments which are used
+
+# New for modification
+testArgs = {}
+expectArgs = {}
+numberCases = 2
+
+newArgs = {}
+newArgs["name"] = "Jane Doe"
+newArgs["username"] = "Janemasterstudy"
+newArgs["lat"] = 123.321
+newArgs["lng"] = 321.123
+newArgs["school"] = "Not plebian school"
+newArgs["courses"] = "Someone cares"
+newArgs["preferences"] = "Other pleasure sessions"
+newArgs["interests"] = "Videos"
+
+# First test case
+testArgs[0] = {}
+testArgs[0]["name"] = "John Doe"
+testArgs[0]["username"] = "JohnStudyMiester"
+testArgs[0]["password"] = "testpasswordthatissupersecure"
+testArgs[0]["deviceid"] = "an_edgey_phone"
+testArgs[0]["lat"] = 123.321
+testArgs[0]["lng"] = 321.123
+testArgs[0]["school"] = "International University of Colleges of Polytechnical University"
+testArgs[0]["courses"] = "Noone cares"
+testArgs[0]["preferences"] = "Quiet,Loud"
+testArgs[0]["interests"] = "Animal Videos"
+# Expected
+expectArgs[0] = {}
+expectArgs[0]["name"] = "John Doe"
+expectArgs[0]["username"] = "JohnStudyMiester"
+expectArgs[0]["lat"] = 123.321
+expectArgs[0]["lng"] = 321.123
+expectArgs[0]["school"] = "International University of Colleges of Polytechnical University"
+expectArgs[0]["courses"] = "Noone cares"
+expectArgs[0]["preferences"] = "Quiet,Loud"
+expectArgs[0]["interests"] = "Animal Videos"
+
+# Second test case
+testArgs[1] = {}
+testArgs[1]["name"] = "Tester McTester"
+testArgs[1]["username"] = "TesterMaster"
+testArgs[1]["password"] = "test password"
+testArgs[1]["deviceid"] = "testdeviceid123"
+testArgs[1]["lat"] = 123.321
+testArgs[1]["lng"] = 321.123
+testArgs[1]["school"] = "Ultrapleb School That Noone goes to"
+testArgs[1]["courses"] = "Noone cares and no one will look"
+testArgs[1]["preferences"] = "Circle Activities"
+testArgs[1]["interests"] = "Wet Sports"
+# Expected
+expectArgs[1] = {}
+expectArgs[1]["name"] = "Tester McTester"
+expectArgs[1]["username"] = "TesterMaster"
+expectArgs[1]["lat"] = 123.321
+expectArgs[1]["lng"] = 321.123
+expectArgs[1]["school"] = "Ultrapleb School That Noone goes to"
+expectArgs[1]["courses"] = "Noone cares and no one will look"
+expectArgs[1]["preferences"] = "Circle Activities"
+expectArgs[1]["interests"] = "Wet Sports"
 
 @pytest.mark.django_db
 class Test:
+    def test_model(self):
+        test0 = createProfile(testArgs[0])
+        uid0 = test0["id"]
+        dhash0 = test0["hash"]
+        uprof0 = Profile.objects.get(pk=uid0)
 
-    def test_create(self):
-        testArgs = {}
-        testArgs["name"] = "Richard Dickenson"
-        testArgs["username"] = "test"
-        testArgs["password"] = "test"
-        testArgs["lat"] = 123.321
-        testArgs["lng"] = 321.123
-        testArgs["school"] = "Plebian school"
-        testArgs["courses"] = "Noone cares"
-        testArgs["preferences"] = "Self pleasure sessions"
-        testArgs["interests"] = "Animal Videos"
-        #print("Set up test arguments")
-        test = createProfile(testArgs)
-        #print(test[id])
-        #if test[id] != -1:
-            #Match.objects.all().filter(Q(user1__exact=test[id])|Q(user2__exact=test[id])).delete()
-        assert True == True
+        assert test0 != None
+        assert uprof0.loggedin == True
+        assert uprof0.__str__() == expectArgs[0]["name"]
+        assert sorted(uprof0.inJson()) == sorted(expectArgs[0])
+
+        assert validID(uid0) == True
+        assert validID(-1) == False
+
+        assert findID(expectArgs[0]["username"])["id"] == uid0
+        assert findID("Thisshouldnotbeausername123asweareinatestdatabase")["id"] == -1
+
+        assert sorted(modifyProfile(newArgs, uid0)) == sorted(newArgs)
+
+        assert uprof0.authenticate(dhash0, testArgs[0]["deviceid"]) == True
+        assert uprof0.authenticate(dhash0, "123") == False
+        assert uprof0.authenticate("123", "123") == False
+        assert uprof0.authenticate("123", testArgs[0]["deviceid"]) == False
+
+        assert uprof0.logout("123", "123")["success"] == False
+        assert uprof0.logout("123", testArgs[0]["deviceid"])["success"] == False
+        assert uprof0.logout(dhash0, "123")["success"] == False
+        assert uprof0.logout(dhash0, testArgs[0]["deviceid"])["success"] == True
+
+    def test_creation_and_string_formats(self):
+        test = {}
+        uid = {}
+        dhash = {}
+        uprof = {}
+        for x in range(0, numberCases - 1):
+            test[x] = createProfile(testArgs[x])
+            uid[x] = test[x]["id"]
+            dhash[x] = test[x]["hash"]
+            uprof[x] = Profile.objects.get(pk=uid[x])
+
+            assert test[x] != None
+            assert uprof[x].loggedin == True
+            assert uprof[x].__str__() == expectArgs[x]["name"]
+            assert sorted(uprof[x].inJson()) == sorted(expectArgs[x])
+
+    def test_validID(self):
+        test = {}
+        uid = {}
+        dhash = {}
+        uprof = {}
+        for x in range(0, numberCases - 1):
+            test[x] = createProfile(testArgs[x])
+            uid[x] = test[x]["id"]
+            dhash[x] = test[x]["hash"]
+            uprof[x] = Profile.objects.get(pk=uid[x])
+
+            assert validID(uid[x]) == True
+
+        assert validID(-1) == False
+
+    def test_findID(self):
+        test = {}
+        uid = {}
+        dhash = {}
+        uprof = {}
+        for x in range(0, numberCases - 1):
+            test[x] = createProfile(testArgs[x])
+            uid[x] = test[x]["id"]
+            dhash[x] = test[x]["hash"]
+            uprof[x] = Profile.objects.get(pk=uid[x])
+
+            assert findID(expectArgs[x]["username"])["id"] == uid[x]
+        assert findID("Thisshouldnotbeausername123asweareinatestdatabase")["id"] == -1
+
+    def test_modify_profile(self):
+        test = {}
+        uid = {}
+        dhash = {}
+        uprof = {}
+        for x in range(0, numberCases - 1):
+            test[x] = createProfile(testArgs[x])
+            uid[x] = test[x]["id"]
+            dhash[x] = test[x]["hash"]
+            uprof[x] = Profile.objects.get(pk=uid[x])
+
+            assert sorted(modifyProfile(newArgs, uid[x])) == sorted(newArgs)
+            assert sorted(uprof[x].inJson()) == sorted(newArgs)
+
+    def test_authenticate(self):
+        test = {}
+        uid = {}
+        dhash = {}
+        uprof = {}
+        for x in range(0, numberCases - 1):
+            test[x] = createProfile(testArgs[x])
+            uid[x] = test[x]["id"]
+            dhash[x] = test[x]["hash"]
+            uprof[x] = Profile.objects.get(pk=uid[x])
+
+            assert uprof[x].authenticate(dhash[x], testArgs[x]["deviceid"]) == True
+            assert uprof[x].authenticate(dhash[x], "Notapassword") == False
+            assert uprof[x].authenticate("Notahash", "Notapassword") == False
+            assert uprof[x].authenticate("Notahash", testArgs[x]["deviceid"]) == False
+
+    def test_logout(self):
+        test = {}
+        uid = {}
+        dhash = {}
+        uprof = {}
+        for x in range(0, numberCases - 1):
+            test[x] = createProfile(testArgs[x])
+            uid[x] = test[x]["id"]
+            dhash[x] = test[x]["hash"]
+            uprof[x] = Profile.objects.get(pk=uid[x])
+            
+            # Is true because user is logged in upon account creation
+            assert uprof[x].loggedin == True
+
+            assert uprof[x].logout("123", "123")["success"] == False
+            assert uprof[x].logout("123", testArgs[x]["deviceid"])["success"] == False
+            assert uprof[x].logout(dhash[x], "123")["success"] == False
+            assert uprof[x].logout(dhash[x], testArgs[x]["deviceid"])["success"] == True
+
+            assert uprof[x].loggedin == False
+
+    def test_login(self):
+        test = {}
+        uid = {}
+        dhash = {}
+        uprof = {}
+        for x in range(0, numberCases - 1):
+            testcaseArg = {}
+
+            test[x] = createProfile(testArgs[x])
+            uid[x] = test[x]["id"]
+            dhash[x] = test[x]["hash"]
+            uprof[x] = Profile.objects.get(pk=uid[x])
+            uprof[x].logout(dhash[x], testArgs[x]["deviceid"])
+            
+            testcaseArg["password"] = "Notapassword"
+            testcaseArg["deviceid"] = "Notadeviceid"
+            assert uprof[x].loggedin == False
+            assert uprof[x].login(testcaseArg)["success"] == False
+            assert uprof[x].loggedin == False
+            uprof[x].logout(dhash[x], testArgs[x]["deviceid"])
+
+            testcaseArg["password"] = "Notapassword"
+            testcaseArg["deviceid"] = testArgs[x]["deviceid"]
+            assert uprof[x].login(testcaseArg)["success"] == False
+            assert uprof[x].loggedin == False
+            uprof[x].logout(dhash[x], testArgs[x]["deviceid"])
+
+            testcaseArg["password"] = testArgs[x]["password"]
+            testcaseArg["deviceid"] = "Notadeviceid"
+            assert uprof[x].login(testcaseArg)["success"] == True
+            assert uprof[x].loggedin == True
+            uprof[x].logout(dhash[x], testArgs[x]["deviceid"])
+
+            testcaseArg["password"] = testArgs[x]["password"]
+            testcaseArg["deviceid"] = testArgs[x]["deviceid"]
+            assert uprof[x].login(testcaseArg)["success"] == True
+            assert uprof[x].loggedin == True
