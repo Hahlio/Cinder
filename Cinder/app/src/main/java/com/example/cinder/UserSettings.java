5e5bbb1f1cc01700362700a54253fec45172938c
Anthony <anthonycsli123@gmail.com>
2018-11-25 16:15:46 -0800

    added api call

--- Cinder/app/src/main/java/com/example/cinder/UserSettings.java
+++ Cinder/app/src/main/java/com/example/cinder/UserSettings.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +13,13 @@ import android.widget.Switch;
 
 import com.facebook.login.LoginManager;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import static com.example.cinder.Signin.getRetro;
+
 public class UserSettings extends AppCompatActivity {
 
     @Override
@@ -19,6 +27,7 @@ public class UserSettings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
         final SharedPreferences mpref = getSharedPreferences("IDValue",0);
+        final int profileID = mpref.getInt("profileID",0);
         Switch notiSwitch  = findViewById(R.id.notiSwitch);
         Button changeProfile = findViewById(R.id.changeProfileButton);
         Button signOut = findViewById(R.id.signOutButton);
@@ -29,6 +38,21 @@ public class UserSettings extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 mpref.edit().putBoolean("noti",b);
+                Retrofit retrofit = getRetro();
+                RestApiCalls apiCalls = retrofit.create(RestApiCalls.class);
+                NotificationSwitch notificationSwitch = new NotificationSwitch();
+                notificationSwitch.setNotify(b);
+                Call<NotificationSwitch> call = apiCalls.setNotification(notificationSwitch,profileID);
+                call.enqueue(new Callback<NotificationSwitch>() {
+                    @Override
+                    public void onResponse(@NonNull Call<NotificationSwitch> call, Response<NotificationSwitch> response) {
+                        //no need for code here
+                    }
+                    @Override
+                    public void onFailure(Call<NotificationSwitch> call, Throwable t) {
+                        //failure code to be written
+                    }
+                });
             }
         });
         changeProfile.setOnClickListener(new View.OnClickListener() {
